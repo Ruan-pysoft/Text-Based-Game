7c3d42de61b17d83ff33ec1d82a20d3e93045cfb
TBBYT <rlschoemankinders@gmail.com>
2019-12-27 19:07:52 +0200

    Moved text to seperate files
    Moved the tutorial and help to seperate files.

--- code/menus.py
+++ code/menus.py
@@ -153,226 +153,15 @@ class Tutorial(Menu):
         super().__init__(name='Load a Game', actions=save.list_saves())
 
     def show(self):
-        print(f'Tutorial:\n ========= \n\n')
-        print(f'''(I suggest playing this tutorial in fullscreen, otherwise you may need to scroll around sometimes)
-
-  When choosing "Start a new game" from the main menu you will be confronted with the following screen:
-    Choose a map:
-     ==========
-
-
-    1) Map 1
-    2) Map 2
-    ...
-    n) Map n
-    Q) Quit to Title
-
-    Choose a map:
-On this screen you are expected to select a map to play on. You select a map by typing the number in front of the ")" and then pressing ENTER. You can also quit to title using "Q" or "q" in stead of a number.
-In this tutorial we will asume you have selected "basic world", one of the two defualt maps that comes with the game, and is map 1 by defualt.
-''')
-        input('Press ENTER to continue... ')
-        clear()
-        print(f'''  When you have selected the "basic world" map you will see the following screen:
-
-            You find yourself in a cave with a flickering torch on the wall.
-            You can make out four paths, each equally as dark and foreboding.
-
-
-    Play Turn 1, Choose an Action:
-
-    d: Move East
-    a: Move West
-    w: Move North
-    s: Move South
-    e: View Inventory
-    r: Rest
-    R: Rest until healed (DANGEROUS IF FIGHTING ENEMY!)
-    stats: View stats
-    Menu: Save/Load/Quit Game
-    Action:
-So what does all this meen? Well, I'll split it up into setions.
-''')
-        input('Press ENTER to discover the sections... ')
-        clear()
-        print(f'''  SECTION I: Room Description:
-
-            You find yourself in a cave with a flickering torch on the wall.
-            You can make out four paths, each equally as dark and foreboding.
-
-  This is just so that you know what the room you are in looks like, and has no pupose except to make the game playable. (Without it, it would be like stumbling around, deaf and blind in the map, only knowing in what direction you can go, what items you have, and how hurt you are)
-
-SECTION II: Play turn:
-
-    Play Turn 1, Choose an Action:
-
-  This only tells you how long you've been playing.
-
-SECTION III: Action:
-
-    d: Move East
-    a: Move West
-    w: Move North
-    s: Move South
-    e: View Inventory
-    r: Rest
-    R: Rest until healed (DANGEROUS IF FIGHTING ENEMY!)
-    stats: View stats
-    Menu: Save/Load/Quit Game
-    Action:
-
-  This is where you do stuff.
-''')
-        input('Press ENTER to find out what stuff you can do... ')
-        clear()
-        print(f'''  So, firstly, you can move around:
-
-    d: Move East
-    a: Move West
-    w: Move North
-    s: Move South
-    Action:
-
-  Let me explain. You're in a world, so let me draw (badly drawn) a map for you:
-____________
-|      !!  |
-|  ¢¢__:(  |
-|    __    |
-|    __    |
-|##__::__$$|
-|    ¢¢    |
-|    __    |
-|    :(    |
-¯¯¯¯¯¯¯¯¯¯¯¯
-  So, to clarify, every two symbols next to each other are one room. So let me give you the map key:
-
-    KEY:
-    :: = Where you start
-       = Nothing, can't go here
-    __ = Empty room
-    :( = Room with a certain enemy in it
-    ¢¢ = Room with only a coin in it
-    $$ = Room with a lot of loot in it
-    ## = Room with an enemy in it that can explode if it has low health
-
-  As you can see, there are rooms surounding you on all sides, and because of that you can move in four directions (you cannot move diagnally). If you were to select "d" (by pressing the "d" key and then pressing ENTER), you would go "left" on this map, and "a" would make you go "right". If you were to select "w" you would go "up", and "s" would make you go "down".
-''')
-        input('Press ENTER to find out what other stuff you can do... ')
-        clear()
-        print(f'''  Secondly, you can view your stats and save/exit your game (or load a new one):
-
-    e: View Inventory
-    stats: View stats
-    Menu: Save/Load/Quit Game
-    Action:
-
-  So if you select "e", you can see what items you have:
-
-    Action: e
-
-    ==========
-    Inventory:
-
-    Silver Coin
-    =====
-    A round coin with 10 stamped on the front.
-    Value: 10
-
-
-    Rock
-    =====
-    A fist-sized rock, suitable for bludgeoning.
-    Value: 0
-    Damage: 6
-
-  As you can see, you start this map with only a silver coin and a rock.
-  Your second option is to view your health and other stats:
-
-    Action: stats
-
-    ==========
-    Health:
-    50/50HP
-    ==========
-    Damage:
-    1.0
-    ==========
-    Damage Resistance:
-    Blunt: 8.9%
-    Sharp: 9.0%
-    Explotion: -31.0%
-    ==========
-
-  As you can see, this game's damage system is not as simple as it may look at first glance (NOTE: Your resistances are randomly generated within a certain range every time you start a new game). It has three types of damage: Blunt damage, Sharpness damage and Explotion damage. You may be confused about the negitive Explotion damage, but all that it means is that if something deals 10 explotion damage to you, your health will go down with 13.1 HP, instead of 9.1 HP, if it was sharpness damage.
-  And lastly, you can save your game, load a old save, or quit to the main menu.
-''')
-        input('Press ENTER to find out how to save/load/quit... ')
-        clear()
-        print(f'''  SAVING YOUR GAME:
-  This is how to save a game:
-
-    1) Select "Menu"
-      Action: Menu
-      Do you want to:
-      1) Save the game
-      2) Load another game
-      3) Quit game
-    2) Select "1"
-      1
-      Give the save a name:
-    3) Choose a name
-      Give the save a name: A Game I Saved
-      Done Saving!
-
-  LOADING A GAME:
-  This is how to load a game:
-
-    1) Select "Menu"
-      Action: Menu
-      Do you want to:
-      1) Save the game
-      2) Load another game
-      3) Quit game
-    2) Select "2"
-      2
-      Saves:
-      1) A Game I Saved
-      Which save do you want to load?
-    3) Choose a save
-      Which save do you want to load? 1
-
-      Play Turn 1, Choose an Action:
-  QUIT TO MAIN:
-  And lastly, how to quit a game:
-
-    1) Select "Menu"
-      Action: Menu
-      Do you want to:
-      1) Save the game
-      2) Load another game
-      3) Quit game
-    2) Select "3"
-      3
-      Do you want to save first?
-      (Y/N)
-    3) Choose "Y" or "N"
-      a.
-        i: Choose "Y":
-          (Y/N) y
-          Give the save a name:
-        ii: Type in a name:
-          Give the save a name: Saving!
-          Title Screen:
-           ===========
-      b.
-        i: Choose "N":
-          (Y/N) n
-          Give the save a name:
-        Title Screen:
-         ===========
-''')
-        input('Press ENTER to quit to title screen... ')
-        clear()
+        with open('./tutorial.info', 'r') as f:
+            tutorial = f.read().split('\n%ENDPAGE%\n')
+        for i, t in enumerate(tutorial):
+            a, b = t.split('%CONTINUE%\n')
+            tutorial[i] = (a, b)
+        for t in tutorial:
+            print(t[0])
+            input(t[1])
+            clear()
 
 class Help(Menu):
     def __init__(self):
@@ -381,14 +170,12 @@ class Help(Menu):
     def show(self):
         import os
         print(f'{self.name}:\n ======= \n\n')
-        tutorial = 0
+        tutorial = -1
         for i, _ in enumerate(TitleScreen.actions):
-            if _.name == 'Play Tutorial':
+            if _.name == 'View Tutorial':
                 tutorial = i
-        print(f'''\
-This is a text-based and turn-based game. It has a Github repository here: https://github.com/TBBYT/Turn-Based-Game
-
-You can view a tutorial by returning to the main menu, and then typing in "{tutorial+1}" and pressing enter.
-''')
+        with open('./help.info', 'r') as f:
+            h = f.read()
+        print(h.format(tutorial+1))
         input('Press ENTER to return to main menu.')
         self.actions[0].method() # Go to main menu
